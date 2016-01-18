d533fea6cd04a97f9cb6049ecd3e27c4bc75ca44
shaunakv1 <shaunakv1@gmail.com>
2016-01-18 12:59:24 -0500

    added function to return file list given a range of numbers

---
+++ pollution.r
@@ -0,0 +1,23 @@
+# function returns a file list as string given a directory
+# and ID of files to fetch
+get_files <- function(directory,id = 1:332) {
+	#fetch all files in the a directory
+	file.names <- list.files(directory)
+  	
+	#from the file names get a list of numbers
+  	file.numbers <- as.numeric(sub('\\.csv$','', file.names))
+  	
+  	#from the number vector match the numbers we need from id arguement
+  	#and use it to select only those file names
+  	selected.files <- na.omit(file.names[match(id, file.numbers)])
+}
+
+# function returns a merged data-frame given a R file list as string
+get_dataframe_from_files <- function(files) {
+
+}
+
+print(get_files("./specdata",1:33))
+
+
+
