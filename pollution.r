73089f05c48385e44325017b8c4116d071c8c468
shaunakv1 <shaunakv1@gmail.com>
2016-01-18 13:16:03 -0500

    fixed the get_files function to return full file paths

--- pollution.r
+++ pollution.r
@@ -2,10 +2,10 @@
 # and ID of files to fetch
 get_files <- function(directory,id = 1:332) {
 	#fetch all files in the a directory
-	file.names <- list.files(directory)
+	file.names <- list.files(directory, full.names = TRUE)
   	
 	#from the file names get a list of numbers
-  	file.numbers <- as.numeric(sub('\\.csv$','', file.names))
+  	file.numbers <- as.numeric(sub('\\.csv$','', basename(file.names)))
   	
   	#from the number vector match the numbers we need from id arguement
   	#and use it to select only those file names
@@ -14,10 +14,9 @@ get_files <- function(directory,id = 1:332) {
 
 # function returns a merged data-frame given a R file list as string
 get_dataframe_from_files <- function(files) {
-
+	df = lapply(files, read.delim)
 }
 
-print(get_files("./specdata",1:33))
-
+file_list <- get_files("./specdata",1:33)
 
 
