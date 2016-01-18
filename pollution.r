71db0d24b06c8a7df15826f40edb83821f9c6c78
shaunakv1 <shaunakv1@gmail.com>
2016-01-18 14:27:18 -0500

    added a function to calculate complete cases in single dataframe

--- pollution.r
+++ pollution.r
@@ -14,12 +14,15 @@ get_files <- function(directory,id = 1:332) {
 
 # function returns a merged data-frame given a R file list as string
 get_dataframe_from_files <- function(files) {
-	#this will return the combined csv files as a list of dataframes
-	#df_list = lapply(files, read.delim)
-	
 	# First apply read.csv, then rbind
 	# This will combine all files into one dataframe
 	myfiles = do.call(rbind, lapply(files, function(x) read.csv(x, stringsAsFactors = FALSE)))
+}
+
+# function returns a list of data-frames given a R file list as string
+get_dataframe_list_from_files <- function(files) {
+	#this will return the combined csv files as a list of dataframes
+	df_list = lapply(files, read.delim)
 }
 
 #this function calculated mean for all values in the given column
@@ -28,20 +31,25 @@ calculate_mean <- function(dataframe, column){
 	m <- mean(dataframe[,column], na.rm = TRUE)
 }
 
+#Function counts the number of complete cases for a dataframe
+complete_cases_count <- function(dataframe){
+	#logical vector of TRUE and FALSES to show which cases are complete
+	l <- complete.cases(data_frame)
+	#count complete (TRUE) cases (values)
+	count <- length(l[l==TRUE])
+}
+
 #this function finds the number of complete observations (none of the columns is NA) in the dataframe and returns the 
 #result as a new dataframe of the form:
 # 	 id nobs
 # 1  2 1041
 # 2  4  474
 
-complete_cases <- function(dataframe){
-	
-}
 
-file_list 	<- get_files("~/code/r/r-experiments/specdata",70:72)
+file_list 	<- get_files("~/code/r/r-experiments/specdata",8)
 data_frame 	<- get_dataframe_from_files(file_list)
 average 	<- calculate_mean(data_frame,'nitrate')
-complete 	<- complete_cases(data_frame)
+complete 	<- complete_cases_count(data_frame)
 
 sprintf("Average : %f",average)
 print("Complete Cases :")
